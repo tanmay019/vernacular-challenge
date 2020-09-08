18664872a7848a6d2138f654e356daa3c9594111
tanmay019 <tanmaygautam999@gmail.com>
2020-09-08 06:59:25 +0530

    working ISO format

--- clean-audio.py
+++ clean-audio.py
@@ -1,18 +1,17 @@
-# find ./ubuntu/audios/ -iname "*.wav" -mtime -60 -print -delete
 import subprocess as sp
 from datetime import datetime
 
 globalCwd = "./ubuntu/audios/"
 
 def run(x, cwd=globalCwd):
-    print("lomdi")
-    print("cd {cwd} && ".format(cwd=cwd)+x)
-    return sp.getoutput("cd {cwd} && ".format(cwd=cwd)+x)
+    command="cd {cwd} && ".format(cwd=cwd)+x
+    # print(command)
+    return sp.getoutput(command)
 
 def printRun(x, cwd=globalCwd):
-    print("bilauta")
-    print("cd {cwd} && ".format(cwd=cwd)+x)
-    print(sp.getoutput("cd {cwd} && ".format(cwd=cwd)+x))
+    command="cd {cwd} && ".format(cwd=cwd)+x
+    # print(command)
+    print(sp.getoutput(command))
 
 x = run('find -iname "*wav" -mmin -2880 -print')
 
@@ -24,9 +23,13 @@ for i in x.split("\n"):
     # printRun('echo '+i)
     # print(i)
     # ----------
+    deletedTime=datetime.now().astimezone().replace(microsecond=0).isoformat()
     # print("echo "+i+" >> deleted-files-"+dd+"-"+mm+"-"+yyyy+".log")
-    printRun("echo "+i+" >> deleted-files-"+dd+"-"+mm+"-"+yyyy+".log", ".")
+    deletedFileName="deleted-files-"+dd+"-"+mm+"-"+yyyy+".log"
+    createdTime=run("date --iso-8601=seconds -r "+i)
+    print(108, createdTime)
+    printRun("echo "+i+" "+deletedTime+" "+createdTime+" >> "+deletedFileName, ".")
     run("rm "+i)
-    print(datetime.now().astimezone().replace(microsecond=0).isoformat())
+    print(deletedTime)
 
 # printRun("touch deleted-files-"+dd+"-"+mm+"-"+yyyy+".log")
